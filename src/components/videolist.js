0fbe03b70bbbb3ceecf4c906e7c1c87733a5c20b
unknown <shivanshkumar166@gmail.com>
2020-11-13 12:31:32 +0530

    showing thumbnails

--- src/components/videolist.js
+++ src/components/videolist.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import VideoItem from './videositem'
+
 const VideosList=props=>{
-    return<div>{props.videos.length}</div>
+
+const renderedList=  props.videos.map((video)=>{
+        return <VideoItem video={video} />
+
+    })
+    return<div>{renderedList}</div>
 }
 export default VideosList
