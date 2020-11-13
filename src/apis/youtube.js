2ce11d1ddd86a3ede1700d3b5770c3e09e0338b0
unknown <shivanshkumar166@gmail.com>
2020-11-14 00:19:18 +0530

    adding final changes

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -4,7 +4,7 @@ export default axios.create({
  baseURL: 'https://www.googleapis.com/youtube/v3',
  params:{
      part:'snippet',
-     maxResult:10,
+    
      key:KEY
 
 
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,13 @@ import VideoList from './videolist'
 import VideoDetail from './VideoDetails'
 class App extends React.Component{
     state={videos:[],selectedVideo:null}
+
+componentDidMount()
+{
+    this.onTermSubmit('React')
+}
+
+
     onTermSubmit=async (term)=>{
 const response= await youtube.get('/search',{
     params:{
@@ -12,7 +19,8 @@ const response= await youtube.get('/search',{
     }
 })
 
-this.setState({videos:response.data.items})
+this.setState({videos:response.data.items,
+selectedVideo:response.data.items[0]})
     }
     onVideoSelect=(video)=>{
        this.setState({selectedVideo:video})
@@ -20,9 +28,14 @@ this.setState({videos:response.data.items})
     render()
     {
         return <div className="ui container"> <SearchBar onFormSubmit={this.onTermSubmit}/> 
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
-
+        <div className="ui grid">
+            <div className="ui row">
+                <div className="eleven wide column">
+        <VideoDetail video={this.state.selectedVideo} /></div>
+        <div className="five wide column">
+        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} /></div>
+        </div>
+        </div>
         </div>
     }
 } 
